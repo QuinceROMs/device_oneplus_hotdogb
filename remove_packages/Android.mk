b774bf4deb917755a6dec54dd873b4935ded03c2
Quince <quinceroms@gmail.com>
2024-12-30 15:20:03 +0100

    remove_packages: Remove FMRadio
    OnePlus 7T has no FM tuner.
    
    Signed-off-by: QuinceROMs <quinceroms@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Accord YouTube Maps Drive Ornament Velvet Gmail2 PersonalSafety FossifyGallery FilesGoogle Chrome64 MusicFX Drive EmergencyInfoGms GoogleFeedback WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES := FMRadio
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
